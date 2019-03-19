23ae1917811baa0bc8f8fa8ad348ba973be0594f
Samuele <ms19.15@localhost.localdomain>
2019-03-19 20:56:30 +0100

    Initial commit

---
+++ ex1/ex1.3/ex1.3.1/sim/compile.sh
@@ -0,0 +1,6 @@
+vcom constants.vhd
+vcom and21_generic.vhd
+vcom or21_generic.vhd
+vcom not_generic.vhd
+vcom mux21_generic.vhd
+vcom tb_mux21_generic.vhd
---
+++ ex1/ex1.3/ex1.3.1/syn/mux21_generic_beh.vhdl
@@ -0,0 +1,39 @@
+
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+
+package CONV_PACK_MUX21_GENERIC_1 is
+
+-- define attributes
+attribute ENUM_ENCODING : STRING;
+
+end CONV_PACK_MUX21_GENERIC_1;
+
+library IEEE;
+
+use IEEE.std_logic_1164.all;
+
+use work.CONV_PACK_MUX21_GENERIC_1.all;
+
+entity MUX21_GENERIC_1 is
+
+   port( A, B : in std_logic_vector (3 downto 0);  SEL : in std_logic;  Y : out
+         std_logic_vector (3 downto 0));
+
+end MUX21_GENERIC_1;
+
+architecture SYN_beh of MUX21_GENERIC_1 is
+
+   component MUX2_X1
+      port( A, B, S : in std_logic;  Z : out std_logic);
+   end component;
+
+begin
+   
+   U10 : MUX2_X1 port map( A => A(3), B => B(3), S => SEL, Z => Y(3));
+   U11 : MUX2_X1 port map( A => A(2), B => B(2), S => SEL, Z => Y(2));
+   U12 : MUX2_X1 port map( A => A(1), B => B(1), S => SEL, Z => Y(1));
+   U13 : MUX2_X1 port map( A => A(0), B => B(0), S => SEL, Z => Y(0));
+
+end SYN_beh;
