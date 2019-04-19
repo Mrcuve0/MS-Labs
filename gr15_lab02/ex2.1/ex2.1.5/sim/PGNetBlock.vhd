6b9a2727dd52334075f19ee8c658b6877e6058fe
Mrcuve0 <cerini.samuele@outlook.it>
2019-04-19 12:35:18 +0200

    Comment the carry generation structure 2.1.5, TODO: comment CSB and CSSG blocks

--- gr15_lab02/ex2.1/ex2.1.5/sim/PGNetBlock.vhd
+++ gr15_lab02/ex2.1/ex2.1.5/sim/PGNetBlock.vhd
@@ -2,10 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+--------------------------------------------------------------------------------
+-- Definition of the "normal" block composing the PG-Network 
+-- (valid for all the blocks composing the PG-Network, except for the very first one)
+--------------------------------------------------------------------------------
+
 entity PGNetBlock is
   port (
     a, b : in  std_logic;
-    gp : out std_logic_vector(1 downto 0));  -- g (MSB), p (LSB)
+    gp : out std_logic_vector(1 downto 0));  -- g (MSB of the vector), p (LSB of the vector)
 end entity PGNetBlock;
 
 -------------------------------------------------------------------------------
@@ -16,7 +21,7 @@ architecture beh of PGNetBlock is
 
 begin  -- architecture beh
 
-  gp(1) <= a and b;
-  gp(0) <= a xor b;
+  gp(1) <= a and b;   -- g
+  gp(0) <= a xor b;   -- p
 
 end architecture beh;
--- gr15_lab02/ex2.1/ex2.1.5/sim/triBlock.vhd
+++ gr15_lab02/ex2.1/ex2.1.5/sim/triBlock.vhd
@@ -2,9 +2,19 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+--------------------------------------------------------------------------------
+-- Definition of the "normal" block composing the first 2 rows of the Sparse Tree
+-- (valid for all the blocks composing the first 2 rows of the ST, 
+-- except for the very first one)
+
+-- This block is internally composed of 3 PG blocks, interconnected together.
+-- As for the previous blocks, each signal follows the bit position defined in the
+-- signal name itself. (G --> MSB, P --> LSB)
+--------------------------------------------------------------------------------
+
 entity triBlock is
   port (
-    Gk_1Pk_1_left  : in  std_logic_vector(1 downto 0);
+    Gk_1Pk_1_left  : in  std_logic_vector(1 downto 0);  
     GikPik_left    : in  std_logic_vector(1 downto 0);
     
     Gk_1Pk_1_right : in  std_logic_vector(1 downto 0);
@@ -26,22 +36,24 @@ architecture struct of triBlock is
       GijPij   : out std_logic_vector(1 downto 0));
   end component GeneralPropagateGenerate;
 
-  signal PG_left_block_out, PG_right_block_out : std_logic_vector(1 downto 0);
+  signal PG_left_block_out, PG_right_block_out : std_logic_vector(1 downto 0); 
+  -- Interconnect the first PG block with the third PG block 
+  -- and the second PG block with the third PG block, respectively
 
 begin  -- architecture struct
 
-  PG_1 : GeneralPropagateGenerate port map (
+  PG_1 : GeneralPropagateGenerate port map (    -- First PG block, receives the inputs from the left 
     Gk_1Pk_1 => Gk_1Pk_1_left,
     GikPik   => GikPik_left,
     GijPij   => PG_left_block_out);
 
-  PG_2 : GeneralPropagateGenerate port map (
+  PG_2 : GeneralPropagateGenerate port map (    -- Second PG block, receives the inputs from the right
     Gk_1Pk_1 => Gk_1Pk_1_right,
     GikPik   => GikPik_right,
     GijPij   => PG_right_block_out);
 
-  PG_3 : GeneralPropagateGenerate port map (
-    Gk_1Pk_1 => PG_right_block_out,
+  PG_3 : GeneralPropagateGenerate port map (    -- Third PG block, receives the inputs from the 2 previous PG blocks
+    Gk_1Pk_1 => PG_right_block_out,             -- Generates the output
     GikPik   => PG_left_block_out,
     GijPij   => GijPij);
 
