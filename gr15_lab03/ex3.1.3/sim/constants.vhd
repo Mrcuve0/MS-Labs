35140c945db342b25ec15f5dc79051ce7f8bdbb7
Mrcuve0 <cerini.samuele@outlook.it>
2019-04-29 10:21:36 +0200

    Add folder structure for Windowed Register File

---
+++ gr15_lab03/ex3.1.3/sim/constants.vhd
@@ -0,0 +1,11 @@
+
+package constants is
+
+  constant numBitData : integer := 64;
+--  constant numRegs    : integer := 32;
+
+  constant numM : integer;    -- Constant for the number of global registers
+  constant numN : integer;    -- Constant for the number of registers in each window 
+  constant numF : integer:    -- Constant for the number of windows
+
+end package constants;
